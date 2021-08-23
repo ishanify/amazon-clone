0d4e7cb80e349b6f79d2b48a6f2f88a206badc62
Ishan <ishandeep999@gmail.com>
2021-08-22 21:30:08 -0400

    pushing prev work to git

---
+++ src/Product.js
@@ -0,0 +1,33 @@
+import "./Product.css"
+import React from 'react'
+
+function Product({ title, image, price, rating }) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐️</p>
+                        ))}
+
+                </div>
+            </div>
+
+            <img className="product__image"
+                src={image} />
+
+            <button>Add to Basket</button>
+        </div>
+
+    )
+}
+
+export default Product
